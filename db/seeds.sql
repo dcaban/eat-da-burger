094836bdee67a8f186904c0f3d6fe130debbe7ab
dcaban <davidjcaban@gmail.com>
2017-11-08 16:22:41 -0500

    database created testing server

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO burgers (burger_name) VALUES ('Yum Yum Burger');
+INSERT INTO burgers (burger_name) VALUES ('Bigmouth Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Joe Burgen', true);
+
